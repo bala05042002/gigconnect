721a422054c021c4522ef5fd1c7bf5856a0c45fc
bala05042002 <19cad216@gmail.com>
2025-08-29 21:22:01 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: ['http://localhost:5173', 'https://gigconnect-zaalima.netlify.app/'],
+        origin: '*',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
     },
 });
